b154c9fcf745c3e68170e3ac21130ab1b083c5c2
TauBai <traumland@hotmail.com>
2016-03-27 10:35:53 +0800

    Update 8.1.1.cc

--- Part-I/8.1.1.cc
+++ Part-I/8.1.1.cc
@@ -1,1 +1,19 @@
 
+#include <iostream>
+std::istream& func(std::istream& instr);
+int main()
+{
+    func(std::cin);
+}
+
+std::istream& func(std::istream& instr)
+{   
+    std::string ss;
+    while(instr >> ss, !instr.eof())
+    {
+        std::cout << ss <<std::endl; 
+    }
+    instr.clear();
+    return instr;   
+    
+}
