91fbe44b8b21422a36d20a3c426e87e0249011ef
TauBai <traumland@hotmail.com>
2016-07-17 00:44:18 +0800

    Create iterator_traits.hpp

---
+++ iterator/iterator_traits.hpp
@@ -0,0 +1,41 @@
+#include <cstddef>
+template <class I>
+struct iterator_traits<I>{
+    typedef typename I::value_type      value_type;
+    typedef typename I::difference_type difference_type;
+    typedef typename I::pointer         pointer;
+    typedef typename I::reference       reference;
+    typedef typename I::iterator_category iterator_category;
+}
+
+//partial specialize non-class type
+
+template <class I>
+struct iterator_traits<I*>{
+    typedef I value_type;
+    typedef ptrdiff_t difference_type;
+//here we can figure out why we have to seperate const type
+    typedef I* pointer;              
+    typedef I& reference;
+}
+template <class I>
+struct iterator_traits<const I*>{
+    typedef I value_type;
+    typedef ptrdiff_t difference_type;
+//here we can figure out why we have to seperate const type
+    typedef const I* pointer;
+    typedef const I& reference;
+}
+
+
+// function count()
+template <class I , class T>
+typename iterator_traits<I>::difference_type 
+count(I first, I last, const T& value){
+    typename iterator_traits<I>::difference_type n = 0;
+    for(; first != last; ++first){
+        if(*first == value)
+            ++n;
+    }
+    return n;
+}
